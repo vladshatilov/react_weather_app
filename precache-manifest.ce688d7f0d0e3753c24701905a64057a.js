081c88405ca563355cb08b1d7ee214f282ac4332
vladshatilov <shatilovvlad@mail.ru>
2020-08-24 19:08:46 +0300

    Updates

---
+++ precache-manifest.ce688d7f0d0e3753c24701905a64057a.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "dfaa414df42af74cef0a464ae9227aa3",
+    "url": "/react_weather_app/index.html"
+  },
+  {
+    "revision": "00444b50dcd8181afa61",
+    "url": "/react_weather_app/static/css/2.d7539782.chunk.css"
+  },
+  {
+    "revision": "87169f4fb29b3f3682bf",
+    "url": "/react_weather_app/static/css/main.e888fd4a.chunk.css"
+  },
+  {
+    "revision": "00444b50dcd8181afa61",
+    "url": "/react_weather_app/static/js/2.20531336.chunk.js"
+  },
+  {
+    "revision": "5ac48c47bb3912b14c2d8de4f56d5ae8",
+    "url": "/react_weather_app/static/js/2.20531336.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "87169f4fb29b3f3682bf",
+    "url": "/react_weather_app/static/js/main.3178e3b0.chunk.js"
+  },
+  {
+    "revision": "ffb2f8abc5547b4dd555",
+    "url": "/react_weather_app/static/js/runtime-main.e979c172.js"
+  }
+]);
